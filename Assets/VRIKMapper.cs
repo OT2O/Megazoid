f39690653fa35eea4f16ffef4c5b0480964ea82b
byrnes-ian <byrnes_ian@bah.com>
2019-06-21 23:50:31 -0700

    initial ik stuff

---
+++ Assets/VRIKMapper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRIKMapper : MonoBehaviour
+{
+    private Animator animator;
+    public Transform target;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1.0f);
+        animator.SetIKPosition(AvatarIKGoal.LeftHand, target.position);
+    }
+
+    void Update()
+    {
+    }
+}
