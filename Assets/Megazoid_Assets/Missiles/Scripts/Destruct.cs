57e5a735ce0a4501bfe46b503bd520c6c3843e79
tysonhinz <hinztyson@gmail.com>
2019-06-22 15:58:27 -0700

    MISSILES!

---
+++ Assets/Megazoid_Assets/Missiles/Scripts/Destruct.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destruct : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+    public float explosionForce = 50.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject.Destroy(gameObject);
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 50.0f);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Rigidbody rBody = colliders[i].GetComponent<Rigidbody>();
+            if(rBody)
+            {
+                rBody.AddExplosionForce(explosionForce, transform.position, 50.0f);
+            }
+        }
+    }
+}
---
+++ Assets/Megazoid_Assets/Missiles/Scripts/RocketFire.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketFire : MonoBehaviour
+{
+    public GameObject missilePrefab;
+    public Transform spawnPoint;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public bool FireMissile(GameObject _missilePrefab)
+    {
+        GameObject usedprefab = missilePrefab;
+        if (_missilePrefab != null)
+        {
+            usedprefab = _missilePrefab;
+        }
+
+        GameObject missile = Instantiate(usedprefab);
+        missile.transform.position = spawnPoint.position;
+        missile.transform.rotation = spawnPoint.rotation;
+
+        Rigidbody rigidboy = missile.GetComponent<Rigidbody>();
+        if (rigidboy == null)
+        {
+            return false;
+        }
+        rigidboy.useGravity = true;
+        rigidboy.isKinematic = false;
+
+        rigidboy.AddRelativeForce(missile.transform.forward * 1000);
+
+        return true;
+    }
+
+}
