24df677f79dacd334e443e7d442a54084a93eee1
Jack <otto_jack@bah.com>
2019-06-23 10:39:35 -0700

    gdhgfdh
    fgh

--- Assets/Megazoid_Assets/Missiles/Scripts/Player.cs
+++ Assets/Megazoid_Assets/Missiles/Scripts/Player.cs
@@ -16,9 +16,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-           rocketFire.FireMissile(missileprefab);
-        } 
+
     }
 }
---
+++ Assets/SetUpNetworkPrefab.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[ExecuteInEditMode]
+public class SetUpNetworkPrefab : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Transform t in transform)
+        {
+            NetworkTransformChild n = gameObject.AddComponent<NetworkTransformChild>();
+            n.target = t;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
