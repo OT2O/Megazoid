8c0f1510e9b64ffc0b60db42be6587ec0e578f6d
Jack <otto_jack@bah.com>
2019-06-22 20:56:47 -0700

    gfhfh
    fgh

--- Assets/PlayerNetworkManager.cs
+++ Assets/PlayerNetworkManager.cs
@@ -4,9 +4,11 @@ using UnityEngine.Networking;
 
 public class PlayerNetworkManager : NetworkBehaviour
 {
-    Transform rightHand, leftHand, rightFoot, leftFoot;
-    public Transform rightHandSphere, leftHandSphere, rightFootSphere, leftFootSphere;
+    Transform myHand;
+    public Transform mySphere;
     public NetworkManager networkManager;
+    public IKMapperSetup ikMapper;
+    public VRIKMapper vrikmapper;
     Transform trackedCalibrators;
     [SyncVar]
     int networkID;
@@ -18,64 +20,75 @@ public class PlayerNetworkManager : NetworkBehaviour
 
         if(isServer)
             networkID = networkManager.numPlayers;
-        
-        trackedCalibrators = GameObject.Find("TrackerCalibrators").transform;
+
+        mySphere = transform.GetChild(0);
+        ikMapper = FindObjectOfType<IKMapperSetup>();
+        vrikmapper = FindObjectOfType<VRIKMapper>();
+
+        if (isLocalPlayer && localPlayerAuthority)
+        {
+            if (networkID == 1)
+            {
+                myHand = GameObject.Find("[CameraRig]").transform.Find("RightHand").transform;
+
+            }
+                
+            else if (networkID == 2)
+            {
+                myHand = GameObject.Find("[CameraRig]").transform.Find("LeftHand").transform;
+
+            }
+               
+            else if (networkID == 3)
+            {
+                myHand = GameObject.Find("[CameraRig]").transform.Find("RightFoot").transform;
+
+            }
+                
+            else if (networkID == 4)
+            {
+                myHand = GameObject.Find("[CameraRig]").transform.Find("LeftFoot").transform;
+            }   
+        }
+
+            if (networkID == 1)
+            {
+
+                ikMapper.rightHandTrackedObject = mySphere;
+                vrikmapper.rightHandTarget = mySphere;
+            }
+
+            else if (networkID == 2)
+            {
+
+                ikMapper.leftHandTrackedObject = mySphere;
+                vrikmapper.leftHandTarget = mySphere;
+            }
+
+            else if (networkID == 3)
+            {
+
+                ikMapper.rightFootTrackedObject = mySphere;
+                vrikmapper.rightFootTarget = mySphere;
+            }
+
+            else if (networkID == 4)
+            {
+                ikMapper.leftFootTrackedObject = mySphere;
+                vrikmapper.leftFootTarget = mySphere;
+
+        }
     }
 
     private void Update()
     {
-        rightHandSphere = trackedCalibrators.Find("RightHandSphere");
-        leftHandSphere = trackedCalibrators.Find("LeftHandSphere");
-        rightFootSphere = trackedCalibrators.Find("RightFootSphere");
-        leftFootSphere = trackedCalibrators.Find("LeftFootSphere");
 
-        rightHand = GameObject.Find("[CameraRig]").transform.Find("RightHand").transform;
-        leftHand = GameObject.Find("[CameraRig]").transform.Find("LeftHand").transform;
-        rightFoot = GameObject.Find("[CameraRig]").transform.Find("RightFoot").transform;
-        leftFoot = GameObject.Find("[CameraRig]").transform.Find("LeftFoot").transform;
 
-        if (isLocalPlayer)
+        if (isLocalPlayer && localPlayerAuthority)
         {
-            if (networkID == 1)
-            {
-                rightHandSphere.localPosition = rightHand.localPosition;
-                //Cmd_UpdateSphere(networkID, rightHandSphere.localPosition);
-            }
-            if (networkID == 2)
-            {
-                leftHandSphere.localPosition = leftHand.localPosition;
-                //Cmd_UpdateSphere(networkID, leftHandSphere.localPosition);
-            }
-            if (networkID == 3)
-            {
-                rightFootSphere.localPosition = rightFoot.localPosition;
-                //Cmd_UpdateSphere(networkID, rightFootSphere.localPosition);
-            }
-            if (networkID == 4)
-            {
-                leftFootSphere.localPosition = leftFoot.localPosition;
-                //Cmd_UpdateSphere(networkID, leftFootSphere.localPosition);
-            }
+            mySphere.localPosition = myHand.localPosition;
         }
     }
-    [Command]
-    private void Cmd_UpdateSphere(int netID, Vector3 localPos)
-    {
-        if (netID == 1)
-        {
-            rightHandSphere.localPosition = localPos;
-        }
-        if (netID == 2)
-        {
-            leftHandSphere.localPosition = localPos;
-        }
-        if (netID == 3)
-        {
-            rightFootSphere.localPosition = localPos;
-        }
-        if (netID == 4)
-        {
-            leftFootSphere.localPosition = localPos;
-        }
-    }
+
+    
 }
