ad36160d95e8dd9a73fabd4dd4f78068314273df
Jack <otto_jack@bah.com>
2019-06-22 21:33:53 -0700

    dfhs
    sfd

--- Assets/PlayerNetworkManager.cs
+++ Assets/PlayerNetworkManager.cs
@@ -5,10 +5,11 @@ using UnityEngine.Networking;
 public class PlayerNetworkManager : NetworkBehaviour
 {
     Transform myHand;
-    public Transform mySphere;
+    public Transform mySphere, myOrigin;
     public NetworkManager networkManager;
     public IKMapperSetup ikMapper;
     public VRIKMapper vrikmapper;
+    public Valve.VR.SteamVR_TrackedObject myDeivce;
     Transform trackedCalibrators;
     [SyncVar]
     int networkID;
@@ -21,71 +22,48 @@ public class PlayerNetworkManager : NetworkBehaviour
         if(isServer)
             networkID = networkManager.numPlayers;
 
+        myOrigin = transform.GetChild(1);
         mySphere = transform.GetChild(0);
         ikMapper = FindObjectOfType<IKMapperSetup>();
         vrikmapper = FindObjectOfType<VRIKMapper>();
 
-        if (isLocalPlayer && localPlayerAuthority)
-        {
-            if (networkID == 1)
-            {
-                myHand = GameObject.Find("[CameraRig]").transform.Find("RightHand").transform;
-
-            }
-                
-            else if (networkID == 2)
-            {
-                myHand = GameObject.Find("[CameraRig]").transform.Find("LeftHand").transform;
-
-            }
-               
-            else if (networkID == 3)
-            {
-                myHand = GameObject.Find("[CameraRig]").transform.Find("RightFoot").transform;
-
-            }
-                
-            else if (networkID == 4)
-            {
-                myHand = GameObject.Find("[CameraRig]").transform.Find("LeftFoot").transform;
-            }   
-        }
-
-            if (networkID == 1)
-            {
-
-                ikMapper.rightHandTrackedObject = mySphere;
-                vrikmapper.rightHandTarget = mySphere;
-            }
-
-            else if (networkID == 2)
-            {
-
-                ikMapper.leftHandTrackedObject = mySphere;
-                vrikmapper.leftHandTarget = mySphere;
-            }
-
-            else if (networkID == 3)
-            {
-
-                ikMapper.rightFootTrackedObject = mySphere;
-                vrikmapper.rightFootTarget = mySphere;
-            }
-
-            else if (networkID == 4)
-            {
-                ikMapper.leftFootTrackedObject = mySphere;
-                vrikmapper.leftFootTarget = mySphere;
-
-        }
+      
     }
 
     private void Update()
     {
+        if (networkID == 1)
+        {
+            ikMapper.rightHandTrackedObject = mySphere;
+            vrikmapper.rightHandTarget = mySphere;
+            ikMapper.rightHandOrigin = myOrigin;
+        }
 
+        else if (networkID == 2)
+        {
+            ikMapper.leftHandTrackedObject = mySphere;
+            vrikmapper.leftHandTarget = mySphere;
+            ikMapper.leftHandOrigin = myOrigin;
+        }
+
+        else if (networkID == 3)
+        {
+            ikMapper.rightFootTrackedObject = mySphere;
+            vrikmapper.rightFootTarget = mySphere;
+            ikMapper.rightFootOrigin = myOrigin;
+        }
+
+        else if (networkID == 4)
+        {
+            ikMapper.leftFootTrackedObject = mySphere;
+            vrikmapper.leftFootTarget = mySphere;
+            ikMapper.leftFootOrigin = myOrigin;
+
+        }
 
         if (isLocalPlayer && localPlayerAuthority)
         {
+            myHand = GameObject.Find("[CameraRig]").transform.Find("Device").transform;
             mySphere.localPosition = myHand.localPosition;
         }
     }
